c55fc4fe586fd97935a70ea2e636f90f915a8260
Rehaan Sharma <rehaansharma164@gmail.com>
2026-03-05 12:27:28 +0530

    Add files via upload

---
+++ JavaBasics.java
@@ -0,0 +1,9 @@
+public class JavaBasics {
+    public static void main(String[] args) {
+        System.out.println("Hello,Basundhara Ji!");  //line space 
+        System.out.println("I love you!");   // line space
+        System.out.println("Hehe!");   // line space
+    }
+}
+
+// boilerplate code: the basic structure of a Java program, which includes the class definition and the main method.
