565a0208387c553859165677d0480cbaef9913b6
Rehaan Sharma <rehaansharma164@gmail.com>
2026-06-09 12:50:29 +0530

    Add files via upload

---
+++ ArithmeticOperators.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class ArithmeticOperators {
+    public static void main(String[] args) {
+        int A = 10 ;
+        int B = 5 ;
+        System.out.println("addition =" + (A + B));
+        System.out.println("subtraction = " + (A - B));
+        System.out.println("multiplication = " + (A * B));
+        System.out.println("division = " + (A / B));
+        System.out.println("modulus = " + (A % B));
+    }}
---
+++ UnaryOperator.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class UnaryOperator {
+    public static void main(String[] args) {
+        // int a = 10 ;
+        // int b = ++a;
+        // System.out.println(a);
+        // System.out.println(b); pre increment
+
+        // int a = 10 ;
+        // int b = a++;
+        // System.out.println(a);
+        // System.out.println(b); post increment
+
+        // int a = 10;
+        // int b = --a;  
+        //  System.out.println(a);
+        // System.out.println(b); pre dencrement
+
+        // int a = 10;
+        // int b = a--;
+        // System.out.println(a);
+        // System.out.println(b); post decrement
+    }
+    
+}
