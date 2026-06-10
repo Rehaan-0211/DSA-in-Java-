3eed68dedfaa2128c1f9c958db576b529a70135c
Rehaan Sharma <rehaansharma164@gmail.com>
2026-06-10 13:08:46 +0530

    Add files via upload

---
+++ RelationalOperator.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class RelationalOperator {
+    public static void main(String[] args) {
+        // int a = 10;
+        // int b = 10;
+        // System.out.println(a==b);
+
+        // int a = 10;
+        // int b = 20;
+        // System.out.println(a!=b);
+
+        // int a = 10;
+        // int b = 5;
+        // System.out.println(a>b);
+        
+        // int a = 10;
+        // int b = 20;
+        // System.out.println(a<b);
+
+        // int a = 10;
+        // int b = 10;
+        // System.out.println(a<=b);  Different types of relational operators.
+    }
+    
+}
