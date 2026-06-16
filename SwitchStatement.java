aedbb4bad88d5665ae5928bcd4096fceed48a4f0
Rehaan Sharma <rehaansharma164@gmail.com>
2026-06-16 21:09:15 +0530

    Add files via upload

---
+++ SwitchStatement.java
@@ -0,0 +1,19 @@
+public class SwitchStatement {
+    public static void main(String[] args) {
+        int number = 4;
+        switch (number) {
+            case 1:
+                System.out.println("Red headed Baddie");
+                break;
+            case 3:
+                System.out.println("Goth mommy");
+                break;
+            case 2:
+                System.out.println(
+                        "Someone who genuinely loves and would do anything but never leave me and would love all parts of me without judging and would happily live with me forever and stay by my side and...damnnn");
+                break;
+            default:
+                System.out.println("Wake up");
+        }
+    }
+}
