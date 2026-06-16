73d42613e0fa5c85e317a5796328c4c706497cfe
Rehaan Sharma <rehaansharma164@gmail.com>
2026-06-16 12:21:49 +0530

    Add files via upload

---
+++ IfElseStatement.java
@@ -0,0 +1,16 @@
+public class IfElseStatement {
+    public static void main(String[] args) {
+        int age = 5;
+        if (age >= 18) {
+            System.out.println ("ADULT : DRIVE,VOTE,DO WTVR U WANNA...kidding.");
+        } 
+        
+        else if ( age >13 && age <18 ) {
+            System.out.println("You are a teenigger.");
+        }
+        
+        else {
+            System.out.println("Kid go watch baby shark..");
+        }
+    }
+}
