755ee0ce54718a467ae7214fdc3d2ae94845031a
Rehaan Sharma <rehaansharma164@gmail.com>
2026-06-16 20:18:29 +0530

    Add files via upload

---
+++ TernaryOperator.java
@@ -0,0 +1,7 @@
+public class TernaryOperator {
+    public static void main(String[] args) {
+        int a = 1;
+        String type = (a%2==0) ? "even" : "odd";
+        System.out.println("The number is " + type);
+    }
+}
