b1c01bf1a01f70c8ad4fec060f70e3421becf4b2
Rehaan Sharma <rehaansharma164@gmail.com>
2026-06-16 15:47:59 +0530

    Add files via upload

---
+++ LargestNumber.java
@@ -0,0 +1,14 @@
+public class LargestNumber {
+    public static void main(String[] args) {
+        int a = 100;
+        int b = 20;
+
+        if (a >= b) {
+            System.out.println("A is the alpha B is the beta");
+            System.out.println("meow meow"); // we can take multiple statements in if and else block by using curly braces. if we dont use curly braces then only the first statement will be considered as part of if and else block. rest of the statements will be executed irrespective of the condition.
+        }
+        else {
+            System.out.println("B is the alpha A is the beta");
+        }
+    }
+}
