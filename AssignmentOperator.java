240b7b0da385881a853a7dfd7b29d591e114ef07
Rehaan Sharma <rehaansharma164@gmail.com>
2026-06-16 11:54:35 +0530

    Add files via upload

---
+++ AssignmentOperator.java
@@ -0,0 +1,12 @@
+public class AssignmentOperator {
+    public static void main(String[] args) {
+        int a = 10;
+        a = a + 10;
+        a += 10; // a = a + 10;
+        a -= 5; // a = a - 5;
+        a *= 2; // a = a * 2;
+        a /= 5; // a = a / 5;
+        a %= 3; // a = a % 3;
+        System.out.println(a);
+    }
+}
