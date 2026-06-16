028d1180c0e95c76aca8d01e31d429b58cacc026
Rehaan Sharma <rehaansharma164@gmail.com>
2026-06-16 20:28:51 +0530

    Add files via upload

---
+++ PassOrFail.java
@@ -0,0 +1,15 @@
+public class PassOrFail {
+    public static void main(String[] args) {
+        int marks = 67;
+        String result = (marks >= 33) ? "pass" : "fail";
+        System.out.println("The nigger has scored " + marks + " and the result is " + result);
+        if (marks == 67)
+        {
+            System.out.println(
+                    "DID SOMEONE SAY 67?....SIX SEVENNNNNNNNNNNNNNN BAHAHAHHAHAHAHHAHAHAHAH..peak use of free will");
+        }
+        else {
+            System.out.println("Nothing special ngl.");
+        }
+    }
+}
