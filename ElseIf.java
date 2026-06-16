fb0fe8e06cbb40630c0b68ca48b9145e10ac5398
Rehaan Sharma <rehaansharma164@gmail.com>
2026-06-16 16:16:01 +0530

    Add files via upload

---
+++ ElseIf.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class ElseIf {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        int age = sc.nextInt();
+        if (age >=18){
+            System.out.println("You are eligible to drive vote do anything...except for drinking and smoking");
+        }
+        else if (age >13 && age <18){
+            System.out.println("You are a teen nigger");
+        }
+        else{
+            System.out.println("Gugugaga");
+        }
+        sc.close();
+    }
+}
