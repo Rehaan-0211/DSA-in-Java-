2c461309db129e04ca17ffd21c8315000c57d076
Rehaan Sharma <rehaansharma164@gmail.com>
2026-06-16 16:45:45 +0530

    Add files via upload

---
+++ LargestNumberAmongThree.java
@@ -0,0 +1,14 @@
+public class LargestNumberAmongThree {
+    public static void main(String[] args) {
+        int a = 10; int b = 2; int c = 3;
+        if (a>=b && a>=c){
+            System.out.println("A is the top g");
+        }
+        else if (b>=c){
+            System.out.println("B is the top g");
+        }
+        else {
+            System.out.println("C is the top g");
+        }
+    }
+}
