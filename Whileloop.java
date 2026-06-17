9d4d3aee1f3e73565c67b5c9f27d3553ac8d155e
Rehaan Sharma <rehaansharma164@gmail.com>
2026-06-17 17:41:48 +0530

    Add files via upload

---
+++ Whileloop.java
@@ -0,0 +1,11 @@
+public class Whileloop {
+    public static void main(String[] args) {
+        int count = 0 ;
+        while (count < 100){
+            System.out.println("Hello Rehaan");
+            count ++; // i lowk forgot to add increment operator and the code ran forever lol XD
+        }
+        
+        System.out.println("Printed it a hundred times not a big deal tbh #nonchalant ");
+    }
+}
