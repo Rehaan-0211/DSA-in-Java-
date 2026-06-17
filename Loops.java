9c542e2288057430288517b71ff6bfb57348f60e
Rehaan Sharma <rehaansharma164@gmail.com>
2026-06-17 17:41:20 +0530

    Add files via upload

---
+++ Loops.java
@@ -0,0 +1,8 @@
+public class Loops {
+    public static void main(String[] args) {
+        System.out.println("Hello Rehaan");
+        System.out.println("Hello Rehaan");
+        //some random guy :- write this a hundred times
+        //Worry not...Rehaan is going to use loops!
+    }
+}
