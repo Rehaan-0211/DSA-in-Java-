11f5537f3e863ac0f0f209a4e349fd7231cb4e53
Rehaan Sharma <rehaansharma164@gmail.com>
2026-06-18 14:45:42 +0530

    Add files via upload

---
+++ BreakStatement.java
@@ -0,0 +1,15 @@
+public class BreakStatement {
+    public static void main(String[] args) {
+        System.out.println("7 is not the part of the matrix...");
+        
+        for(int i =1;i<=10;i++){
+             if (i==7){
+                break;
+             }   
+            
+            System.out.println(i);
+            
+        }
+            System.out.println("Im gone...");
+    }
+}
