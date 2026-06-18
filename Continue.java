b8fe42876bd0ab83553443c1914cdeabb8d85d8a
Rehaan Sharma <rehaansharma164@gmail.com>
2026-06-18 15:15:40 +0530

    Add files via upload

---
+++ Continue.java
@@ -0,0 +1,10 @@
+public class Continue {
+    public static void main(String[] args) {
+        for(int i = 1;i<=10;i++){
+            if(i==7){
+                continue;
+            }
+            System.out.println("You won't ever get to see 7 again..bro left the matrix after all...(those who know)");
+        }
+    }
+}
