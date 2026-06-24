531f043a38746a3f9628fdf7cd71a406a3e5c2fb
Rehaan Sharma <rehaansharma164@gmail.com>
2026-06-24 14:44:50 +0530

    Add files via upload

---
+++ PractiseProblem_5.java
@@ -0,0 +1,18 @@
+//(Operator phase) Question 2 :-  Dry run the following code
+
+public class PractiseProblem_5{
+    public static void main(String[] args) {
+        int x = 200, y = 50, z = 100;
+        if(x > y && y > z){
+            System.out.println("Hello");
+        }
+        if(z > y && z < x){
+            System.out.println("Java");
+        
+        }if((y+200) < x && (y+150) < z){
+            System.out.println("Hello Java");
+        }
+    }
+}
+
+// My answer is java and...im correct
