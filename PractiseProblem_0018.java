b2e5137fac076642d45e3080e9c2a0a127e22a32
Rehaan Sharma <rehaansharma164@gmail.com>
2026-08-05 09:54:30 +0530

    Add files via upload

---
+++ PractiseProblem_0018.java
@@ -0,0 +1,10 @@
+public class PractiseProblem_0018 {
+    public static void main(String[] args){
+        //1 - 50 even numbers
+        for(int i = 1;i<=50;i++){
+            if(i%2 == 0){
+                System.out.println(i);
+            }
+        }
+    }
+}
