2a89e9a099e5f766e0188305b3d761922a3f69d8
Rehaan Sharma <rehaansharma164@gmail.com>
2026-08-05 09:52:52 +0530

    Add files via upload

---
+++ PractiseProblem_0017.java
@@ -0,0 +1,9 @@
+public class PractiseProblem_0017 {
+    public static void main(String[] args){
+        int i = 10;
+        while (i<11 && i>0) {
+            System.out.print(" " + i);
+            i--;
+        }
+    }
+}
